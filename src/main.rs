a78c7097cca7444d6a87be61c7a7ee4db03759dd
Asjid Kalam <asjid.kalam@gmail.com>
2021-07-10 21:49:02 +0530

    added rayon

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,52 @@
 mod banner;
 
-// use instant::Instant;
+use instant::Instant;
 use lazy_static::lazy_static;
 use md5::Digest;
+use rayon::prelude::*;
 use std::env;
 use std::process;
-use std::sync::Mutex;
-use std::thread;
+
+use indicatif::ProgressBar;
 
 /*
     example hash(MD5):
     e890f806dfd189052ca7b39ac29da142 -> PASSWORD1
 */
 
-// fn crack(wordlist_string: &mut String) -> String {
-//     // let wordlist_string = wordlist_string.unwrap().clone();
-//     let hashvalue = md5::Md5::digest(wordlist_string.as_bytes());
-//     format!("{:x}", hashvalue)
-// }
-
 lazy_static! {
-    static ref HASH_DICT: Mutex<Vec<String>> = Mutex::new(vec![]);
     static ref HASH_INPUT: String = env::args().nth(2).unwrap();
 }
 
-fn crack() {
-    // parallization
-    HASH_DICT.lock().iter().for_each(|lines| {
-        for line in lines.iter() {
-            thread::spawn(move || {
-                let wordlist_string = line.trim();
-                let hashvalue = md5::Md5::digest(wordlist_string.as_bytes());
-                let formatted_hash = format!("{:x}", hashvalue);
+static mut LOAD_TIME: u128 = 0;
 
-                if formatted_hash == *HASH_INPUT {
-                    println!("🤍 Cracked! {} -> {}", formatted_hash, wordlist_string);
-                    process::exit(0);
-                }
-            });
+fn crack(line: String, now: std::time::Instant) {
+    let hashvalue = md5::Md5::digest(line.as_bytes());
+    let formatted_hash = format!("{:x}", hashvalue);
+
+    if formatted_hash == *HASH_INPUT {
+        unsafe {
+            println!(
+                "🤍 Cracked! {} -> \"{}\" in {} millisecs",
+                formatted_hash,
+                line,
+                now.elapsed().as_millis() - LOAD_TIME
+            );
         }
-    });
+        process::exit(0);
+    }
 }
 
 fn main() -> std::io::Result<()> {
     banner::display_banner();
 
-    // let now = Instant::now();
+    let mut hash_dict: Vec<String> = vec![];
 
     // TODO: use a real fucking argument parser.
     let args: Vec<String> = env::args().collect();
     let wordlist_file = &args[1];
 
-    // println!("📋 Wordlist: {}", wordlist_file);
+    println!("📋 Wordlist: {}", wordlist_file);
 
     // TODO: check if the hash is already cracked in the local crack repository and also in the online repository.
     // local repository: ~/.ruo/ruo.saved
@@ -59,12 +54,27 @@ fn main() -> std::io::Result<()> {
     let mut reader = my_reader::BufReader::open(wordlist_file)?;
     let mut buffer = String::new();
 
-    while let Some(line) = reader.read_line(&mut buffer) {
-        // HASH_DICT.lock().unwrap().push(line?.trim().to_string());
-    }
-    println!("loaded the wordlist file.");
+    let now = Instant::now();
 
-    crack();
+    while let Some(line) = reader.read_line(&mut buffer) {
+        hash_dict.push(line?.trim_end().to_string());
+    }
+
+    unsafe {
+        LOAD_TIME = now.elapsed().as_millis();
+        println!("loaded the wordlist file in {} millisecs.", LOAD_TIME);
+    }
+
+    let pb = ProgressBar::new(hash_dict.len() as u64);
+
+    for _ in 0..hash_dict.len() {
+        hash_dict.par_iter().for_each(|lines| {
+            let line = lines.clone();
+            crack(line, now);
+        });
+        pb.inc(1);
+    }
+    pb.finish_with_message("Done!");
 
     Ok(())
 }
